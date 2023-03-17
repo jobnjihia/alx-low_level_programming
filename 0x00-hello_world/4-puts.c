fcc6f3a2de27c0fbbeead5cdc87e8055b9e8a0e3
jobnjihia <jobnjihia09@gmail.com>
2023-03-16 17:01:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Puts function prints string
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
