c0c4f82b61cd3a6bbe5d1c3a0bd66cab4c9acdb8
jobnjihia <jobnjihia09@gmail.com>
2023-03-19 09:18:08 -0700

    prints the alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the aplphabet with exception of q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+		return (0);
+}
