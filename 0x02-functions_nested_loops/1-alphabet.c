57b3370b4d15e2ad44089db5542ac85b61e65fae
jobnjihia <jobnjihia09@gmail.com>
2023-03-22 04:52:37 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ * @c: The character to print
+ * Return: On success 0.
+ */
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	return (0);
+}
