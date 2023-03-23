d4a8aaf695861dce84b7482137cecfe79ec77431
jobnjihia <jobnjihia09@gmail.com>
2023-03-23 08:44:29 -0700

    checks if character is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase characters
+ * @c: checked character
+ * Return: 1 otherwise return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= "Z")
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
