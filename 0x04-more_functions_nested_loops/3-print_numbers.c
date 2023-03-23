ba75a38dc235e242e51b831a8ffb6a6aea0afdb1
jobnjihia <jobnjihia09@gmail.com>
2023-03-23 14:14:27 -0700

    prints integers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,12 @@
  * print_numbers - prints the numbers 0 to 9
  * Return: void
  */
-
+int _putchar(int c);
 void print_numbers(void)
 {
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
