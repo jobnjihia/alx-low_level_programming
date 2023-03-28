38fb3216ceed8d89c2e6fa1bc89c85ccd7974240
jobnjihia <jobnjihia09@gmail.com>
2023-03-28 10:51:59 -0700

    prints string characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ * Return: Void
+ */
+void puts2(char *str)
+{
+	int a;
+	int b = 0;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+	for (a = 0; a < b; a += 2)
+	{
+		_putchar('\n')
+	}
+}
