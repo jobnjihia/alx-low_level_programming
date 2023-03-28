bdd5e0dc257395dca57cd74b2974a4c587fe4876
jobnjihia <jobnjihia09@gmail.com>
2023-03-28 07:25:35 -0700

    updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
+#include "main.h"
 #include "stdio.h"
 /**
  * main -  updates the value it points to to 98
  * @n: Integer input pointer
  * Return: Always 0
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
