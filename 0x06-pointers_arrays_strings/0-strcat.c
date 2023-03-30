8d8b1aef5d54af48395a9bf2859abd7f6aad248f
jobnjihia <jobnjihia09@gmail.com>
2023-03-30 15:59:59 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,26 +3,24 @@
 /**
  * _strcat - Concatenates two strings
  * @dest: Destination
- * @char: Bytes from source
  * @src: Source
  * Return: char
  */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+	char *s = dest;
 
-	while (*(dest + count) != '\0')
+	while (*dest != '\0')
 	{
-		count++;
+		dest++;
 	}
 
-	while (count2 >= 0)
+	while (*src != '\0')
 	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	return (dest);
+	*dest = '\0';
+	return (s);
 }
