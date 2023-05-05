ad0dceff403be5d07047e49618ce8c195f418613
jobnjihia <jobnjihia09@gmai.com>
2023-05-04 19:06:55 -0700

    bit manupilation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,24 +2,20 @@
 /**
  * binary_to_uint - changes binary number to an unsigned integer
  * @b: input string cointaining binary
+ * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
 	int m;
-	unsigned int n = 0, l = 1;
+	unsigned int n = 0;
 
 	if (!b)
 		return (0);
-	for (m = 0; b[m] != '\0';)
-		m++;
-	for (m -= 1; m >= 0; m--)
-		{
-			if (b[m] != '0' && b[m] != '1')
-				return (0);
-			if (b[m] != '0' && b[m] != '1')
-				return (0);
-			n += (b[m] - '0') * l;
-			l *= 2;
-		}
+	for (m = 0; b[m]; m++)
+	{
+		if (b[m] < '0' || b[m] > '1')
+			return (0);
+		n = 2 * n + (b[m] - '0');
+	}
 	return (n);
 }
